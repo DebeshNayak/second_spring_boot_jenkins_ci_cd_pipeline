0bed2d17ae2e412529abb010a230e52a8f9daac7
student <dnayak382@gmail.com>
2021-02-27 17:10:10 +0000

    initial commit

---
+++ src/main/java/com/debesh/mysecondcicd/MySecondCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.debesh.mysecondcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySecondCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySecondCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/debesh/mysecondcicd/MySecondCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debesh.mysecondcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySecondCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
